5ff13573f3acf95f136acfe7de78b49b3a78204a
Emilis Dambauskas <emilis.d@gmail.com>
2010-07-12 11:08:42 +0300

    start-crawler.js: added (re)indexing of items.

--- modules/start-crawler.js
+++ modules/start-crawler.js
@@ -9,6 +9,12 @@ require("load-gluestick");
 // Load additional RingoJS packages:
 require("packages").loadPackages(getRepository( require("config").PACKAGES_DIR ));
 
+var JsonStorage = require("ctl/JsonStorage");
+var SolrClient = require("PolicyFeed/SolrClient");
+
+JsonStorage.addTrigger("after-write", "/docs/", SolrClient.onItemChange);
+JsonStorage.addTrigger("after-remove", "/docs/", SolrClient.onItemChange);
+
 var slist = require("PolicyFeed/SourceList");
 slist.init(require("config").PolicyFeed.SourceList);
 slist.checkUpdates();
