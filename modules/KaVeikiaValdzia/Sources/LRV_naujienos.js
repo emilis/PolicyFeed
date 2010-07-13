c52b3923f7fc6d20022dbba32e751c0898a5c87e
Emilis Dambauskas <emilis.d@gmail.com>
2010-07-13 16:24:58 +0300

    Rewritten Sources/LRV_naujienos as per new architecture.

--- modules/KaVeikiaValdzia/Sources/LRV_naujienos.js
+++ modules/KaVeikiaValdzia/Sources/LRV_naujienos.js
@@ -19,24 +19,72 @@
 
 require("core/date");
 
-// Extends:
-exports.extendObject("PolicyFeed/Source");
-
-exports.name = "LRV_naujienos";
-exports.short_name = "LRV-NA";
 exports.index_url = "http://www.lrv.lt/rss/rss.php?cats=1-9-3-2-7-6-8-10-11-12-13-14-15-16-17-18-19-20-21-22-23&lang=lt";
+/*
+exports.domains = {
+    "lrv.lt": 5*60*1000
+};
+*/
 
-exports.doc_template = {
-    source: exports.name,
-    short_source: exports.short_name
-    };
+
+var UrlList = require("PolicyFeed/UrlList");
+var JsonStorage = require("ctl/JsonStorage");
+var Sequence = require("ctl/SimpleSequence");
+var htmlunit = require("htmlunit");
 
 
 /**
  *
  */
-exports.parseIndexItem = function(item)
-{
+exports.error = function(method, msg) {
+    return Error(this.name + ".method Error: " + msg);
+}
+
+
+/**
+ *
+ */
+exports.checkIndex = function(url, page) {
+    //todo: add checks to ensure that the index is a valid RSS file.
+
+    if (!page.getByXPath)
+        throw this.error("checkIndex", page); 
+    
+    var items = page.getByXPath("/rss/channel/item").toArray();
+    if (items.length < 1)
+        throw this.error("checkIndex", "No RSS items found in index.");
+
+    items = items.map(this.parseIndexItem).filter(this.removeExisting);
+
+    // todo: move this to a separate method.
+    // create originals:
+    for each (var item in items) {
+        var id = "/originals/" + item.published.substr(0, 10).replace(/-/g, "/") + "/";
+        id += Sequence.next();
+
+        print("adding page:", id, item.published, item.url, item.title);
+
+        JsonStorage.write(id, item);
+        UrlList.addUrl(item.url, id);
+
+        this.UrlQueue.addUrl({
+            url: item.url,
+            source: this.name,
+            method: "parsePage",
+            original_id: id
+            });
+    }
+    
+    // schedule next check:
+    this.UrlQueue.scheduleUrl(url, new Date(new Date().getTime() + 5*60*1000));
+}
+
+
+
+/**
+ *
+ */
+exports.parseIndexItem = function(item) {
     var result = {
         url:        item.getFirstByXPath("link").asText(),
         title:      item.getFirstByXPath("title").asText(),
@@ -44,27 +92,64 @@ exports.parseIndexItem = function(item)
         summary:    item.getFirstByXPath("description").asText()
         };
 
+    result.source = this.name;
+
     result.published = new Date(result.published).format("yyyy-MM-dd HH:mm:ss");
     result.published = result.published.replace(/00:00:00/, new Date().format("HH:mm:ss"));
+
+    result.type = "pranesimas";
+    result.organization = "Vyriausybė";
     
     return result;
 }
 
+
 /**
  *
  */
-exports.extractDocumentData = function(doc)
-{
-    if (doc.meta.converted_by)
+exports.removeExisting = function(item) {
+    if (UrlList.exists(item.url))
         return false;
+    else
+        return true;
+}
 
-    var page = this.htmlunit.getPageFromHtml(doc.html, doc.meta.url, this.name);
-    this.htmlunit.fixPageUrls(page);
-    var result = {};
+
+/**
+ *
+ */
+exports.parsePage = function(url, page) {
+    var original = JsonStorage.read(url.original_id);
+
+    var response = page.getWebResponse();
+    original.content_type = response.getContentType();
+
+    // Sgml page is parent class for HtmlPage, XmlPage and XhtmlPage.
+    if (page instanceof com.gargoylesoftware.htmlunit.SgmlPage)
+    {
+        htmlunit.setPageCharset(page, "UTF-8");
+        original.html = page.asXml();
+    }
+    else
+        throw this.error("parsePage", url.original_id + " | " + url.url);
+
+    // save original:
+    JsonStorage.write(original._id, original);
+
+
+    //---
+
+    // create doc from original:
+    var doc = original;
+    doc._id = doc._id.replace("originals", "docs");
+
+    // Warning: No updates to original after this point or you'll regret it.
+
+    htmlunit.fixPageUrls(page);
 
     // title:
-    if (!doc.meta.title)
-        result.title = page.getFirstByXPath('//div[@class="title"]/dl/dt').asText();
+    if (!doc.title)
+        doc.title = page.getFirstByXPath('//div[@class="title"]/dl/dt').asText();
 
     // html:
     var news_div = page.getFirstByXPath('//div[@class="news-view"]');
@@ -77,7 +162,12 @@ exports.extractDocumentData = function(doc)
     if ((extra_div = news_div.getLastChild()) && extra_div.getTagName && (extra_div.getTagName() == "div"))
         extra_div.remove();
 
-    result.html = news_div.asXml();
+    doc.html = news_div.asXml();
 
-    return result;
+    // save doc:
+    JsonStorage.write(doc._id, doc);
+
+    return true;
 }
+
+
