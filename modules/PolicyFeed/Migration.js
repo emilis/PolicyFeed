9bef76e40944a9d7457382c7d322a72391dd0645
Emilis Dambauskas <emilis.d@gmail.com>
2010-07-28 01:50:10 +0300

    Reimplemented PolicyFeed/Migration script.

--- modules/PolicyFeed/Migration.js
+++ modules/PolicyFeed/Migration.js
@@ -19,33 +19,26 @@
 
 require("core/date");
 
-var db = loadObject("DB");
-var new_db = loadObject("ctl/JsonStorage");
+var DB_old = loadObject("DB_old");
+var JsonStorage = require("ctl/JsonStorage");
+var Sequence = require("ctl/SimpleSequence");
+
 // original_id to new added_id:
-exports.oid2addid = {};
-// original_id to doc_id:
-exports.oid2did = {};
+exports.oid2new = {};
 
+// old-source -> type map:
+var typemap = {
+    LRS_darbotvarkes: "darbotvarke",
+    LRS_pranesimai_spaudai: "pranesimas",
+    LRS_teises_aktai: "projektas",
+    LRV_darbotvarkes: "darbotvarke",
+    LRV_naujienos: "pranesimas"
+};
 
-exports.migrate = function() {
-
-    print(new Date());
-    this.docs();
-
-    
-    print(new Date());
-    this.originals();
-
-    /*
-    print(new Date());
-    this.create_listings();
-
-    print(new Date());
-    this.comments();
-    */
-
-    print(new Date());
-}
+var orgs = {
+    LRS: ["Seimas", "Lietuvos Respublikos Seimas"],
+    LRV: ["Vyriausybė", "Lietuvos Respublikos Vyriausybė"]
+};
 
 
 // Create special chars RegExp:
@@ -62,26 +55,42 @@ var fixString = function(str) {
     return str.replace(special_chars, " ").replace(/\s+/g, " ").trim();
 }
 
+//----------------------------------------------------------------------------
+
+/**
+ * Migrate everything.
+ */
+exports.migrate = function() {
+
+    print(new Date());
+
+    this.originals();
+
+    print(new Date());
+
+    this.docs();
+
+    print(new Date());
+}
+
 
 /**
  *
  */
 exports.docs = function(ids) {
-
-    //var sql = "select id,original_id,comment_count,meta,html, convert_tz(updated,'SYSTEM','+00:00') as `update`, convert_tz(published,'SYSTEM','+00:00') as `publish` from docs order by published asc";
     if (ids === undefined)
         var sql = "select * from docs order by published asc";
     else
         var sql = "select * from docs where id in('" + ids.join("','") + "')";
 
-    var rs = db.query(sql);
+    var rs = DB_old.query(sql);
     if (!rs.first())
         return false;
 
     rs.beforeFirst();
     while (rs.next())
     {
-        var doc = db.get_row(rs);
+        var doc = DB_old.get_row(rs);
         try {
             var data = JSON.parse(doc.meta);
         } catch (e) {
@@ -89,8 +98,8 @@ exports.docs = function(ids) {
             print(e);
         }
 
-        data.id             = doc.id;
-        data.original_id    = doc.original_id;
+        //data.id             = doc.id;
+        //data.original_id    = doc.original_id;
         data.updated        = doc.updated;
         data.published      = doc.published;
         data.comment_count  = doc.comment_count;
@@ -99,8 +108,12 @@ exports.docs = function(ids) {
         data.title = fixString(data.title);
         data.html  = fixString(data.html);
 
-        var id = "/docs/" + data.published.substr(0, 10).replace(/-/g, "/") + "/" + data.original_id + "/doc";
-        new_db.write(id, data);
+        data.type = typemap[data.source];
+        data.org = orgs[data.source.substr(0, 3)][0];
+        data.organization = orgs[data.source.substr(0, 3)][1];
+
+        var id = oid2new[data.original_id].replace("originals", "docs");
+        JsonStorage.write(id, data);
     }
 
     rs.getStatement().close();
@@ -111,17 +124,16 @@ exports.docs = function(ids) {
  *
  */
 exports.originals = function() {
-    //var sql = "select id,source,url,meta,html, convert_tz(updated,'SYSTEM','+00:00') as `update`, convert_tz(published,'SYSTEM','+00:00') as `publish` from originals";
     var sql = "select * from originals";
 
-    var rs = db.query(sql);
+    var rs = DB_old.query(sql);
     if (!rs.first())
         return false;
 
     rs.beforeFirst();
     while (rs.next())
     {
-        var original = db.get_row(rs);
+        var original = DB_old.get_row(rs);
         try {
             var data = JSON.parse(original.meta);
         } catch (e) {
@@ -129,44 +141,21 @@ exports.originals = function() {
             print(e);
         }
 
-        data.id         = original.id;
         data.updated    = original.updated;
         data.published  = original.published;
-        data.source     = original.source;
+        data.parser     = original.source;
         data.url        = original.url
         data.html       = original.html;
 
-        var id = "/originals/" + data.published.substr(0, 10).replace(/-/g, "/") + "/" + data.id + "/doc";
-        new_db.write(id, data);
-    }
+        data.type = typemap[original.source];
+        data.org = orgs[original.source.substr(0, 3)][0];
+        data.organization = orgs[original.source.substr(0, 3)][1];
 
-    rs.getStatement().close();
-}
+        var id = "/originals/" + data.published.substr(0, 10).replace(/-/g, "/") + "/" + original.id;
 
+        JsonStorage.write(id, data);
 
-/**
- *
- */
-exports.create_listings = function() {
-    
-    var sql = "select docs.id as doc_id, docs.convertion as convertion, convertions.created as updated, convertions.body as body, docs.comment_count as comment_count from docs, convertions where convertions.id = docs.convertion";
-
-    var rs = db_new.query(sql);
-    if (!rs.first())
-        return false;
-
-    rs.beforeFirst();
-    while (rs.next())
-    {
-        var doc = db_new.get_row(rs);
-        doc.body= JSON.parse(doc.body);
-
-        var sql = "insert into doc_listing (doc_id, convertion, published, updated, title, source, comment_count) values(?,?,?,?,?,?,?)";
-
-        if (doc.body.title.length > 255)
-            doc.body.title = doc.body.title.substr(0, 252) + "...";
-
-        db_new.prepared_query(sql, [doc.doc_id, doc.convertion, doc.body.published, doc.updated, doc.body.title, doc.body.short_source, doc.comment_count] );
+        oid2new[original.id] = id;
     }
 
     rs.getStatement().close();
@@ -178,7 +167,7 @@ exports.create_listings = function() {
  */
 exports.comments = function() {
 
-    var sql=" insert into policyfeed.comments (updated, parent_id, thread_id, author, user_id, score, text) (select updated, parent_id, thread_id, author, user_id, score, text from govsrvr.comments)";
+    var sql="select updated, parent_id, thread_id, author, user_id, score, text from govsrvr.comments";
 
-    return db_new.query(sql);
+    var rs = db.query(sql);
 }
