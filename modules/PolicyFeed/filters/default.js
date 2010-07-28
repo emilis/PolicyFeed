c02499bf52c4651d7fbd5e7c450ed46425dec13a
Emilis Dambauskas <emilis.d@gmail.com>
2010-07-28 13:24:51 +0300

    Improved default HTML filter. Added filterXml() method to work on DomElements.

--- modules/PolicyFeed/filters/default.js
+++ modules/PolicyFeed/filters/default.js
@@ -24,14 +24,40 @@ var remove = [
     "<font[^>]*>",
     "</font>"];
 
-var replace = [
-    [ /<([^>]+)\slang="[^"]*"/gi, "<$1"],
-    [ /<([^>]+)\sstyle="[^"]*"/gi, "<$1"],
-    [ /<([^>]+)\sdir="[^"]*"/gi, "<$1"],
-    [ /<([^>]+)\sclass="[^"]*"/gi, "<$1"],
-    [ /<([^>]+)\sid="[^"]*"/gi, "<$1"],
-    [ /<([^>]+)\salign="justify"/gi, "<$1"] ];
 
+var attr_start = "<([^>]+)\\s";
+var attr_end = '="[^"]*"';
+var attr_remove = "<$1";
+
+var replace = [
+    [ /<([^>]+)\salign="justify"/gi, attr_remove],
+    [ /<pre[^>]*>/gi, '<div class="pre">']
+    ];
+
+
+var attributes = ["lang", "style", "dir", "class", "id"];
+
+var html5_events = ["onafterprint", "onbeforeprint", "onbeforeonload", "onblur", "onerror", "onfocus", "onhaschange", "onload", "onmessage", "onoffline", "ononline", "onpagehide", "onpageshow", "onpopstate", "onredo", "onresize", "onstorage", "onundo", "onunload", "onblur", "onchange", "oncontextmenu", "onfocus", "onformchange", "onforminput", "oninput", "oninvalid", "onreset", "onselect", "onsubmit", "onkeydown", "onkeypress", "onkeyup", "onclick", "ondblclick", "ondrag", "ondragend", "ondragenter", "ondragleave", "ondragover", "ondragstart", "ondrop", "onmousedown", "onmousemove", "onmouseout", "onmouseover", "onmouseup", "onmousewheel", "onscroll", "onabort", "oncanplay", "oncanplaythrough", "ondurationchange", "onemptied", "onended", "onerror", "onloadeddata", "onloadedmetadata", "onloadstart", "onpause", "onplay", "onplaying", "onprogress", "onratechange", "onreadystatechange", "onseeked", "onseeking", "onstalled", "onsuspend", "ontimeupdate", "onvolumechange", "onwaiting"]
+
+var ie_events = ["onactivate", "onafterupdate", "onbeforeactivate", "onbeforecopy", "onbeforecut", "onbeforedeactivate", "onbeforeeditfocus", "onbeforepaste", "onbeforeunload", "onbeforeupdate", "onbounce", "oncontrolselect", "oncopy", "oncut", "ondataavailable", "ondatasetchanged", "ondeactivate", "ondragend", "ondragleave", "ondragstart", "onerror", "onerrorupdate", "onfilterchange", "onfinish", "onfocusin", "onfocusout", "onhelp", "onlayoutcomplete", "onlosecapture", "onmouseenter", "onmouseleave", "onmove", "onmoveend", "onmovestart", "onpaste", "onpropertychange", "onreadystatechanged", "onresizeend", "onresizestart", "onrowenter", "onrowexit", "onrowsdelete", "onrowsinserted", "onselectionchange", "onstart", "onstop", "ontimeerror"
+
+
+var replace_attribute = function(attr) {
+    replace.push([
+        new RegExp(attr_start + attr + attr_end, "gi"),
+        attr_remove
+        ]);
+}
+
+attributes.map(replace_attribute);
+html5_events.map(replace_attribute);
+ie_events.map(replace_attribute);
+
+
+
+/**
+ *
+ */
 exports.filter = function(html) {
 
     for each (var re in remove)
@@ -45,3 +71,30 @@ exports.filter = function(html) {
 }
 
 
+var remove_item = function(item) {
+    item.remove();
+}
+
+/**
+ *
+ */
+exports.filterXml = function(html) {
+    // remove script elements:
+    html.getByXPath("//script").toArray().map(remove_item);
+    
+    // remove style tags:
+    html.getByXPath("//style").toArray().map(remove_item);
+
+    /* 
+    var remove_empty = ["b", "strong", "i", "em", "u", "sup", "sub", "pre", "p", "div"];
+
+    remove_empty.map(function (tag) {
+            html.getByXPath("//" + tag).toArray().map(function (element) {
+                if (!element.asText().trim())
+                    element.remove();
+            });
+        });
+    */
+    
+    return html;
+}
