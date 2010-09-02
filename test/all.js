d946de060d15696b1bf8187aee83bfecc978de0a
Emilis Dambauskas <emilis.d@gmail.com>
2010-09-02 04:44:57 +0300

    Added some tests for config.

--- test/all.js
+++ test/all.js
@@ -1,8 +1,9 @@
 
-exports.testAssert = require('./assert');
+exports.testAssert = require("./assert");
+exports.testConfig = require("./config");
 exports.testCtlJsonStorage = require("./ctl/JsonStorage");
 
 // start the test runner if we're called directly from command line
 if (require.main == module.id) {
-    require('test').run(exports);
+    require("test").run(exports);
 }
