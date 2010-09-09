5ead09a7d51ef8a76a9f61612ad2d139045e218d
Emilis Dambauskas <emilis.d@gmail.com>
2010-09-09 19:12:40 +0300

    Added proper copyright notice to organization tag module.

--- modules/KaVeikiaValdzia/tags/organizations.js
+++ modules/KaVeikiaValdzia/tags/organizations.js
@@ -1,3 +1,21 @@
+/*
+    Copyright 2010 Emilis Dambauskas
+
+    This file is part of KąVeikiaValdžia.lt website.
+
+    KąVeikiaValdžia.lt is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KąVeikiaValdžia.lt is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with KąVeikiaValdžia.lt.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 // Requirements:
