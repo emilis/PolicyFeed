1ec523b6d532fb91640255315c5389369353933a
Emilis Dambauskas <emilis.d@gmail.com>
2010-10-19 07:13:24 +0300

    Added start scripts for debugging.

---
+++ bin/debug-www.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+ROOT_DIR=$(dirname $(cd $(dirname "$0"); pwd););
+
+LIB_DIR="$ROOT_DIR/lib";
+CONFIG_DIR="$ROOT_DIR/config";
+MODULES_DIR="$ROOT_DIR/modules";
+
+# set monitor mode for resuming java job from background
+set -o monitor
+
+java \
+    -Dringo.classpath="./**"\
+    -jar "$LIB_DIR/run.jar"\
+    -D ringo.home="$LIB_DIR/ringo.jar"\
+    -e "require.paths.push('$CONFIG_DIR')"\
+    -di "$MODULES_DIR/start-www.js" &
+
+# save pid of java process:
+echo $! > "$ROOT_DIR/data/www.pid"
+
+# bring java process to foreground:
+fg
