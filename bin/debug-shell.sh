cc4e01c2b57ed13ca322a308d1f975c8c1d792a1
Emilis Dambauskas <emilis.d@gmail.com>
2011-01-29 12:42:52 +0200

    Added a script to start a shell with debugger.

---
+++ bin/debug-shell.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+ROOT_DIR=$(dirname $(cd $(dirname "$0"); pwd););
+
+LIB_DIR="$ROOT_DIR/lib";
+CONFIG_DIR="$ROOT_DIR/config";
+MODULES_DIR="$ROOT_DIR/modules";
+
+java \
+    -Dringo.classpath="./**"\
+    -jar "$LIB_DIR/run.jar"\
+    -D ringo.home="$LIB_DIR/ringo.jar"\
+    -e "require.paths.push('$CONFIG_DIR')"\
+    -di "$MODULES_DIR/start-shell.js"
+
